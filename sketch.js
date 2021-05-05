07349934a675f4e34d6af40f2627bb13e798660b
shivapro <75524510+shivapro@users.noreply.github.com>
2021-05-05 15:05:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -43,41 +43,41 @@ async function getBackgroundImg(){
         
         if(hour>=06 && hour<=08){
             bg = "sunrise1.png";
-        }
+        }else
         if(hour>=08 && hour<=10){
             bg = "sunrise2.png";
-        };
+        }else
         if(hour>=10 && hour<=12){
             bg = "sunrise3.png";
-        };
+        }else
         if(hour>=12 && hour<=14){
             bg = "sunrise4.png";
-        }
+        }else
         if(hour>=16 && hour<=18){
             bg = "sunrise5.png";
-        }
+        }else
         if(hour>=18 && hour<=19){
             bg = "sunrise6.png";
-        }
+        }else
         if(hour>=19&& hour<=20){
             bg = "sunset7.png";
-        }
+        }else
         if(hour>=20 && hour<=22){
             bg = "sunrise8.png";
-        }
+        }else
         if(hour>=22 && hour<=01){
             bg = "sunset9.png";
-        };
+        }else
         if(hour>=01 && hour<=02){
             bg = "sunset10.png";
-        }
+        }else
         if(hour>=02 && hour<=04){
             bg = "sunset11.png";
-        }
+        }else
         if(hour>=04 && hour<=06){
             bg = "sunrise1.png";
-        }
+        };
     
         backgroundImg = loadImage(bg);
-        console.log(backgroundImg);
+        //console.log(background);
 }
